9ea53170b7a0dce498204919db2d4c48b64b72ee
edalzell <erin@silentz.co>
2021-12-15 12:09:12 -0800

    queue the notifications

--- src/Notifications/AbstractAlert.php
+++ src/Notifications/AbstractAlert.php
@@ -3,10 +3,11 @@
 namespace TransformStudios\Uptime\Notifications;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Notification;
 use Statamic\Support\Arr;
 
-abstract class AbstractAlert extends Notification
+abstract class AbstractAlert extends Notification implements ShouldQueue
 {
     use Queueable;
 
